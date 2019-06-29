b0f72eb97090eee17c03865700fa53eb0184079e
Anatol333 <bolshoymedvedtoliano@gmail.com>
2019-06-29 23:30:34 +0300

    prepearing db test

--- db_script.sql
+++ db_script.sql
@@ -15,11 +15,11 @@ drop table if exists Trains;
 CREATE TABLE Users
 (
 	id                   INTEGER NOT NULL auto_increment,
-	login                INTEGER unique NOT NULL,
+	login                VARCHAR(20) unique NOT NULL,
 	password             VARCHAR(20) NOT NULL,
 	email                VARCHAR(50) unique NOT NULL,
-	name                 VARCHAR(20) NOT NULL,
-	surname              VARCHAR(20) NOT NULL,
+	name                 VARCHAR(20),
+	surname              VARCHAR(20),
 	PRIMARY KEY (id)
 );
 
@@ -76,6 +76,7 @@ CREATE TABLE Cities
 (
 	id_country           Integer(20) NOT NULL,
 	id                   INTEGER NOT NULL auto_increment,
+    name                 VARCHAR(20) NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY (id_country) REFERENCES Countries (id)
 );
@@ -100,8 +101,8 @@ CREATE TABLE Routes
 CREATE TABLE routes_station
 (
 	id_station           INTEGER NOT NULL,
-	time_start           DATE NOT NULL,
-	time_end             DATE NULL,
+	time_start           DATETIME NOT NULL,
+	time_end             DATETIME NULL,
 	price                INTEGER NULL,
 	id_route             INTEGER NOT NULL,
 	id_train             INTEGER NOT NULL,
@@ -112,12 +113,12 @@ CREATE TABLE routes_station
 
 CREATE TABLE routes_on_date
 (
-	date_start           DATE NOT NULL,
+	date_start           DATETIME NOT NULL,
 	id_station           INTEGER NOT NULL,
 	id_route             INTEGER NOT NULL,
 	id_train             INTEGER NOT NULL,
-	time_date_start      DATE NULL,
-	time_date_end        DATE NULL,
+	time_date_start      DATETIME NULL,
+	time_date_end        DATETIME NULL,
 	PRIMARY KEY (date_start,id_station,id_route,id_train),
 	FOREIGN KEY (id_station, id_route, id_train) REFERENCES routes_station (id_station, id_route, id_train)
 );
@@ -129,13 +130,64 @@ CREATE TABLE user_check
 	id_train             INTEGER NOT NULL,
 	num_carriage         INTEGER NOT NULL,
 	num_seat             INTEGER NOT NULL,
-	date_start           DATE NOT NULL,
+	date_start           DATETIME NOT NULL,
 	id_station           INTEGER NOT NULL,
 	id_route             INTEGER NOT NULL,
-	date_start_station   DATE NOT NULL,
-	date_end_station     DATE NULL,
+	date_start_station   DATETIME NOT NULL,
+	date_end_station     DATETIME NULL,
 	PRIMARY KEY (id_user,date_start_station,id_train,num_carriage,num_seat,id_station,id_route),
 	FOREIGN KEY (id_user) REFERENCES Users (id),
 	FOREIGN KEY (num_carriage, num_seat, id_train) REFERENCES Seats (num_carriage, num_seat, id_train),
 	FOREIGN KEY (date_start, id_station, id_route, id_train) REFERENCES routes_on_date (date_start, id_station, id_route, id_train)
 );
+
+INSERT INTO users (login,password,email) VALUES('admin','admin','admin@mail.com');
+INSERT INTO user_roles(id, role) VALUES (1,'USER');
+INSERT INTO user_roles(id, role) VALUES (1,'ADMIN');
+
+INSERT INTO countries (name) values ('Ukraine');
+INSERT INTO cities(id_country, name) values (1, 'Odessa');
+INSERT INTO cities(id_country, name) values (1, 'Poltava');
+INSERT INTO cities(id_country, name) values (1, 'Kharkov');
+INSERT INTO cities(id_country, name) values (1, 'Kyiv');
+INSERT INTO cities(id_country, name) values (1, 'Lviv');
+
+-- Adding Station test info --  
+INSERT INTO Stations(id_city,name) VALUES(1, 'Odessa-Holovna');
+INSERT INTO Stations(id_city,name) VALUES(1, 'Odessa-Mala');
+INSERT INTO Stations(id_city,name) VALUES(2, 'Poltava-Pivdenna');
+INSERT INTO Stations(id_city,name) VALUES(3, 'Kharkiv');
+
+-- Adding Train test info --  
+INSERT INTO trains(number) values(263);
+
+INSERT INTO types(name, price) values('common', 100);
+
+INSERT INTO carriages(num_carriage, id_train, id_type) VALUES(1,1,1);
+INSERT INTO carriages(num_carriage, id_train, id_type) VALUES(2,1,1);
+INSERT INTO carriages(num_carriage, id_train, id_type) VALUES(3,1,1);
+
+INSERT INTO seats(id_train, num_carriage,num_seat) VALUES(1,1,1);
+INSERT INTO seats(id_train, num_carriage,num_seat) VALUES(1,1,2);
+INSERT INTO seats(id_train, num_carriage,num_seat) VALUES(1,1,3);
+INSERT INTO seats(id_train, num_carriage,num_seat) VALUES(1,1,4);
+INSERT INTO seats(id_train, num_carriage,num_seat) VALUES(1,1,5);
+
+INSERT INTO seats(id_train, num_carriage,num_seat) VALUES(1,2,1);
+INSERT INTO seats(id_train, num_carriage,num_seat) VALUES(1,2,2);
+INSERT INTO seats(id_train, num_carriage,num_seat) VALUES(1,2,3);
+INSERT INTO seats(id_train, num_carriage,num_seat) VALUES(1,2,4);
+INSERT INTO seats(id_train, num_carriage,num_seat) VALUES(1,2,5);
+
+INSERT INTO seats(id_train, num_carriage,num_seat) VALUES(1,3,1);
+INSERT INTO seats(id_train, num_carriage,num_seat) VALUES(1,3,2);
+INSERT INTO seats(id_train, num_carriage,num_seat) VALUES(1,3,3);
+INSERT INTO seats(id_train, num_carriage,num_seat) VALUES(1,3,4);
+INSERT INTO seats(id_train, num_carriage,num_seat) VALUES(1,3,5);
+
+-- Adding Route test info --  
+INSERT INTO routes(id, id_train) VALUES(1,1);
+
+INSERT INTO routes_station(id_train, id_route, id_station, time_start, time_end, price) VALUES(1,1,1, '2019-7-18 17:34:00', '2019-7-18 17:34:00', 20);
+INSERT INTO routes_station(id_train, id_route, id_station, time_start, time_end, price) VALUES(1,1,2, '2019-8-18 06:30:00', '2019-8-18 06:34:00', 70);
+INSERT INTO routes_station(id_train, id_route, id_station, time_start, time_end, price) VALUES(1,1,3, '2019-8-18 08:22:00', '2019-8-18 08:22:00',30);
